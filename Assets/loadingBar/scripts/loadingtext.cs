487378b0cc714ea5430899d96cded220174b2668
Patrick Vasile <p.vasile45@gmail.com>
2018-03-12 15:34:56 -0400

    Classroom Scene Building, Loading Screen Implemented but needs to be edited

---
+++ Assets/loadingBar/scripts/loadingtext.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadingtext : MonoBehaviour {
+
+    private RectTransform rectComponent;
+    private Image imageComp;
+
+    public float speed = 100f;
+    public Text text;
+    public Text textNormal;
+	public string levelToLoad;
+	public Canvas loadCanvas;
+
+
+    // Use this for initialization
+    void Start () {
+        rectComponent = GetComponent<RectTransform>();
+        imageComp = rectComponent.GetComponent<Image>();
+        imageComp.fillAmount = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int a = 0;
+        if (imageComp.fillAmount != 1f)
+        {
+            imageComp.fillAmount = imageComp.fillAmount + Time.deltaTime * speed;
+            a = (int)(imageComp.fillAmount * 100);
+            if (a > 0 && a <= 33)
+            {
+                textNormal.text = "Loading...";
+            }
+
+			if (a >= 100)
+			{
+				loadCanvas.enabled = false;
+			}
+            else if (a > 33 && a <= 67)
+            {
+                textNormal.text = "Downloading...";
+            }
+            else if (a > 67 && a <= 100)
+            {
+                textNormal.text = "Please wait...";
+            }
+            else {
+
+            }
+            text.text = a + "%";
+        }
+        else
+        {
+            imageComp.fillAmount = 0.0f;
+            text.text = "0%";
+        }
+    }
+}
